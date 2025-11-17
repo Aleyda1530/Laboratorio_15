c4a5ebd88888fa17c93e5f516778c4ff95a57269
Aleyda <aquispehach@unsa.edu.pe>
2025-11-17 17:10:41 -0500

    Ejercicio 6: Modificar los atributos de una imagen

---
+++ js/Ejercicio5.js
@@ -0,0 +1,6 @@
+// Ejercicio 5 - Cambiar atributos de la primera imagen <img>
+// Autor : Aleyda Quispe
+
+var imagen = document.querySelector("img");
+imagen.setAttribute("src", "imagenes/producto2.png")
+imagen.setAttribute("alt", "Imagen actualizada desde JS");
