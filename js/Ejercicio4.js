b8f4dc3092d834efa2b17a0d2e937c5d67db7d9f
Aleyda <aquispehach@unsa.edu.pe>
2025-11-17 16:59:46 -0500

    Ejercicio 4: Actualizar el texto de todos los parrafos

---
+++ js/Ejercicio4.js
@@ -0,0 +1,7 @@
+// Ejercicio 4 - Actualizar el contenido de todos los párrafos <p>
+// Autor : Aleyda Quispe
+
+var parrafos = document.querySelectorAll("p");
+for (var i = 0; i < parrafos.length; i++){
+    parrafos[i].textContent = "Nuevo texto actualizado";
+}
