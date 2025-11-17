d9a87a3642308e1760cec00714a8207e9870b96d
Aleyda <aquispehach@unsa.edu.pe>
2025-11-17 16:48:52 -0500

    Ejercicio 3: Seleccionar y mostrar primer h2

---
+++ js/Ejercicio3.js
@@ -0,0 +1,7 @@
+// Ejercicio 3 - Seleccionar el primer h2 y mostrar su texto
+// Autor : Aleyda Quispe
+
+var primerh2 = document.querySelector("h2");
+var texto = primerh2.textContent;
+
+console.log(texto);
