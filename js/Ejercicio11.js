eabf1c7b6fa83a04dc9a8b66f07d83e205424a06
Aleyda <aquispehach@unsa.edu.pe>
2025-11-17 21:22:14 -0500

    Ejercicio 11: Correccion a ejercicio para agregar 5 li

--- js/Ejercicio11.js
+++ js/Ejercicio11.js
@@ -1,6 +1,11 @@
-// Ejercicio 11 - Clonar un párrafo y agregarlo al final del body
+// Ejercicio 11 - Crear un <ul> y agregar 5 <li>, luego insertar la lista dentro de un <section> existente
 // Autor : Aleyda Quispe
 
-var parrafo = document.querySelector("#parrafoOriginal");
-var clonParrafo = parrafo.cloneNode(true);
-document.body.appendChild(clonParrafo);
+var lista = document.createElement("ul");
+for(var i = 0; i < 5; i++){
+    var li = document.createElement("li");
+    li.textContent = "Elemento " + (i+1);
+    lista.appendChild(li);
+}
+var seccion = document.getElementById("seccionExistente");
+seccion.appendChild(lista);
