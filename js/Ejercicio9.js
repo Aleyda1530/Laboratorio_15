4e1d5086a0d763d7d01e53ca562f863081af4669
Aleyda <aquispehach@unsa.edu.pe>
2025-11-17 20:41:41 -0500

    Ejercicio 9: Crear un párrafo nuevo en JS y agregarlo al body

---
+++ js/Ejercicio9.js
@@ -0,0 +1,6 @@
+// Ejercicio 9 - Crear un párrafo y añadirlo al final del body
+// Autor : Aleyda Quispe
+
+var nuevoParrafo = document.createElement("p");
+nuevoParrafo.textContent="Este es el nuevo elemento";
+document.body.appendChild(nuevoParrafo);
