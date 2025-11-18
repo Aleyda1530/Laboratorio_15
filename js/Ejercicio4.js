ea07519997a20dd373e29a1ae013b1fefabbfbee
Aleyda <aquispehach@unsa.edu.pe>
2025-11-18 09:39:08 -0500

    Ejercicio 4: Actualizar el contenido de todos los párrafos - Corregido

--- js/Ejercicio4.js
+++ js/Ejercicio4.js
@@ -3,5 +3,5 @@
 
 var parrafos = document.querySelectorAll("p");
 for (var i = 0; i < parrafos.length; i++){
-    parrafos[i].textContent = "Nuevo texto actualizado";
+    parrafos[i].textContent = "Texto actualizado dinamicamente";
 }
